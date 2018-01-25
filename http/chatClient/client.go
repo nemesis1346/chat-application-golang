f8d17268cfaa80b4a5dc8b0d852693aedf3e0021
nemesis1346 <marco.maigua1346@gmail.com>
2018-01-25 15:40:08 -0600

    getting the general structure client http

--- http/chatClient/client.go
+++ http/chatClient/client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -18,4 +20,66 @@ func main() {
 
 	body, err := ioutil.ReadAll(resp.Body)
 	fmt.Println(body)
+
+	//Introduce credentials
+	fmt.Println("Enter a username: ")
+	username := bufio.NewReader(os.Stdin)
+
+	inputUserObject, err := username.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+	inputUserObject = inputUserObject[:len(inputUserObject)-1]
+	userObject := UsernameStruct{
+		Username: string(inputUserObject)}
+
+	for {
+
+		//Introduce options
+		option := bufio.NewReader(os.Stdin)
+
+		//Interface for options of the client
+		fmt.Println("Choose an action:")
+		fmt.Println("1.Create a chatroom")
+		fmt.Println("2.List all existing chatrooms ")
+		fmt.Println("3.Join a chatroom ")
+		fmt.Println("4.Leave a chatroom \n")
+
+		fmt.Print("Choose option: ")
+		//reading the input
+		input, _, err := option.ReadRune()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		//OPTIONS
+		switch input {
+		case '1':
+			createChatRoom(userObject)
+		case '2':
+			listChatRoom(userObject)
+		case '3':
+			joinChatRoom(userObject)
+		case '4':
+			leaveChatRoom(userObject)
+		}
+	}
+
+}
+
+func createChatRoom(userObject UsernameStruct) {
+	//HTTP POST OR GET
+}
+func listChatRoom(userObject UsernameStruct) {
+
+}
+func joinChatRoom(userObject UsernameStruct) {
+
+}
+func leaveChatRoom(userObject UsernameStruct) {
+
+}
+
+type UsernameStruct struct {
+	Username string `json:"username"`
 }
