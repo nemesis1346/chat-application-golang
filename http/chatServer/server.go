2a46b4709e7dd77605eccce9bad5e2163f1c365f
nemesis1346 <marco.maigua1346@gmail.com>
2018-01-25 15:13:19 -0600

    server testing

--- http/chatServer/server.go
+++ http/chatServer/server.go
@@ -5,19 +5,55 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/http/httptest"
+	. "testing"
 )
+
+//type message struct{}
+type test int
 
 func main() {
 	fmt.Println("Starting server...")
 	h := http.NewServeMux()
 
-	h.HandleFunc("/endpoint1", getMessage)
+	//DEFINITION OF METHODS
+	h.HandleFunc("/endpoint1", GetMessage)
+	h.HandleFunc("/", NoEndpoint)
 
+	//LISTEN AND ERRORS
 	err := http.ListenAndServe(":8888", h)
 	log.Fatal(err)
+
+	//test := TestNumberDumper(1)
+}
+
+//Endpoint nil
+func NoEndpoint(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(404)
+	fmt.Fprintln(w, "You are lost, go home")
 }
 
 //My first endpoint
-func getMessage(w http.ResponseWriter, req *http.Request) {
+func GetMessage(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "hello, world")
+
+}
+
+//FOR TESTING
+func TestNumberDumper(t *T) {
+	n := test(1)
+	r, _ := http.NewRequest("GET", "/endpoint1", nil)
+	w := httptest.NewRecorder()
+	n.ServeHTTP(w, r)
+	if w.Code != 200 {
+		t.Fatalf("wrong code returned: %d", w.Code)
+	}
+	body := w.Body.String()
+	if body != fmt.Sprintf("Here is your number: 1\n") {
+		t.Fatalf("Wrong body returned: %s", body)
+	}
+}
+
+func (m test) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Here's your number: %d\n", m)
 }
