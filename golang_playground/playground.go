dd800d228340c8f8c1cad698a7b77c11f6ff0e8a
nemesis1346 <marco.maigua1346@gmail.com>
2024-10-30 19:54:19 -0400

    go playgorund

---
+++ golang_playground/playground.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func main(){
+	
+	
+	fmt.Println("Hola Mundo")
+	
+	// For condicional 
+	for i:=0; i<=10; i++{
+		fmt.Println(i)
+	}
+	
+	fmt.Println('\n')
+	
+	// For while 
+	counter: = 0
+	for counter <10{
+		fmt.Println(counter)
+		counter++
+	}
+
+} 
